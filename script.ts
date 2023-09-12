5a5cb0936264d58df7495b186c3b1ec9575083a4
Nate <nethanelstahlberg@gmail.com>
2023-09-12 13:41:57 +0300

    findUnique

--- script.ts
+++ script.ts
@@ -2,20 +2,10 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.deleteMany();
-  const users = await prisma.user.createMany({
-    data: [
-      {
-        name: "Kyle",
-        email: "kyle@gmail.com",
-        age: 27,
-      },
-      {
-        name: "Sally",
-        email: "sally@gmail.com",
-        age: 32,
-      },
-    ],
+  const users = await prisma.user.findUnique({
+    where: {
+      email: "kyle@gmail.com",
+    },
   });
   console.log(users);
 }
